9166ce3888b9cf9af2503a436d5c7c71ee6a4d22
Eden Full <spacecamper@gmail.com>
2016-07-24 17:55:03 -0400

    Getting rid of double press bug

--- dashDetect.py
+++ dashDetect.py
@@ -2,18 +2,21 @@ import logging # for the following line
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR) # suppress IPV6 warning on startup
 from scapy.all import * # for sniffing for the ARP packets
 from subprocess import call
-import getData
+import getData, datetime
 
 # it takes a minute for the scapy sniffing to initialize, so I print this to know when it's actually ready to go
 print('Init done.')
 
+lastCalled = datetime.datetime.today()
+
 def arp_display(pkt):
   if pkt[ARP].op == 1: #who-has (request)
-    if pkt[ARP].psrc == '0.0.0.0': # ARP Probes will match this
+    if (pkt[ARP].psrc == '0.0.0.0') and (datetime.datetime.now() - lastCalled > datetime.datetime.timedelta(minutes=1)): # ARP Probes will match this
         weather = getData.getWeather()
         subway = getData.getSubway()
 
         if pkt[ARP].hwsrc == 'f0:27:2d:86:7b:bf':
+            lastCalled = datetime.datetime.now()
             call(['flite', '-voice', 'slt', getData.getDateTime()])
             call(['flite', '-voice', 'slt', weather['morning']])
             call(['flite', '-voice', 'slt', weather['day']])
